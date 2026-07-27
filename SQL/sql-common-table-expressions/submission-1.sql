4dce97d564efedeb7a046b8f8d734decd89854fe
Ray <161912464+rzhang2004@users.noreply.github.com>
2026-07-27 16:49:51 -0400

    Add: sql-common-table-expressions - submission-1

---
+++ SQL/sql-common-table-expressions/submission-1.sql
@@ -0,0 +1,55 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+WITH small_orders AS (
+    SELECT customer_id, MAX(price)
+    FROM orders
+    GROUP BY customer_id, id
+    HAVING MAX(price) < 100
+)
+
+SELECT c.name
+FROM customers AS c
+JOIN small_orders AS s
+    ON c.id = s.customer_id
+ORDER BY c.name;
+
+
+
+
+
+
+
+
+
+
+

e2b87a595d1d80373f38c63c01d8a6342a9d8ba3
Ray <161912464+rzhang2004@users.noreply.github.com>
2026-08-24 22:27:49 -0500

    Add: sql-auto-increment - submission-2

---
+++ SQL/sql-auto-increment/submission-2.sql
@@ -0,0 +1,21 @@
+CREATE SEQUENCE gov_id START WITH 1000 INCREMENT BY 3;
+
+CREATE TABLE gov_employee (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  gov_id INT DEFAULT nextval('gov_id'),
+  name TEXT
+);
+
+
+
+
+
+
+-- Do not modify below this line --
+INSERT INTO gov_employee (name) 
+  VALUES
+      ('John Doe'),
+      ('Jane Doe'),
+      ('Jim Beam');
+
+SELECT * FROM gov_employee;
